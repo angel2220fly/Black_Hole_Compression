fe7dddbaf1b91ed3bc1fb79deaa7512286cccad2
JURJIUS PACALOVAS <56896464+angel2220fly@users.noreply.github.com>
2024-03-13 13:18:54 +0000

    Update Black_Hole_Quantum_1.0.0.0.py

--- Black_Hole_Quantum_1.0.0.0.py
+++ Black_Hole_Quantum_1.0.0.0.py
@@ -16,7 +16,7 @@ class compression:
                         Before_X = Number_of_the_file
                         Square_of_ROOT = Hole_Number_information
                         
-                        if  Square_of_ROOT<=26*1024*1024
+                        if  Square_of_ROOT>=26*1024*1024
                                    Square_of_ROOT=26*1024*1024
                             
                         if Multiply>26*1024*1024:
